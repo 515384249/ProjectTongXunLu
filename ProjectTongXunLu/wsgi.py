8de65db5edc458078b48babe55906ba797611953
515384249 <515384249@qq.com>
2019-06-27 10:23:20 +0800

    部署前的代码

--- ProjectTongXunLu/wsgi.py
+++ ProjectTongXunLu/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectTongXunLu.settings')
 
 application = get_wsgi_application()
+sys.path.append("/srv/tongxunlu/ProjectTongXunLu/")
